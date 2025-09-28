7f0b115688d031f9826715622f0c52314e2cbb56
DanielS <newds57066@gmail.com>
2025-09-28 17:34:34 +0300

    579A - выращиваем бактерии

---
+++ 1000/579A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int x, ans = 0;
+    cin >> x;
+    while (x > 0) {
+        if (x & 1) {
+            ans++;
+        }
+        x /= 2;
+    }
+    cout<<ans<<endl;
+    return 0;
+}
